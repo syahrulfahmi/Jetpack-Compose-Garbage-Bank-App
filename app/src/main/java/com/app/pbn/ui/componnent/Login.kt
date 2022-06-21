b749d340ee3d2be4465c6d223116d99290902f83
syahrulfahmi <syahrulfahmi331@gmail.com>
2022-06-21 21:15:05 +0700

    add state password text field

--- app/src/main/java/com/app/pbn/ui/componnent/Login.kt
+++ app/src/main/java/com/app/pbn/ui/componnent/Login.kt
@@ -2,12 +2,18 @@ package com.app.pbn.ui.componnent
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Button
-import androidx.compose.material.ButtonDefaults
-import androidx.compose.material.OutlinedTextField
-import androidx.compose.material.Text
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.ArrowForward
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -15,6 +21,9 @@ import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -24,11 +33,13 @@ import com.app.pbn.R
 @Composable
 fun Login() {
     var username by remember { mutableStateOf("") }
-    var pass by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    var passwordVisibility: Boolean by remember { mutableStateOf(false) }
 
     Column(
         modifier = Modifier
             .fillMaxSize()
+            .verticalScroll(rememberScrollState())
             .background(colorResource(id = R.color.white)),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
@@ -63,14 +74,28 @@ fun Login() {
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(
-                    top = dimensionResource(id = R.dimen.padding_8),
+                    top = dimensionResource(id = R.dimen.padding_16),
                     start = dimensionResource(id = R.dimen.padding_16),
                     end = dimensionResource(id = R.dimen.padding_16)
                 ),
             shape = RoundedCornerShape(30),
-            value = pass,
-            onValueChange = { pass = it },
-            label = { Text("Masukan password") }
+            value = password,
+            onValueChange = { password = it },
+            label = { Text("Enter password") },
+            visualTransformation = if (passwordVisibility) VisualTransformation.None else PasswordVisualTransformation(),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+            trailingIcon = {
+                IconButton(onClick = {
+                    passwordVisibility = !passwordVisibility
+                }) {
+                    Icon(
+                        imageVector = if (passwordVisibility)
+                            Icons.Filled.Visibility
+                        else
+                            Icons.Filled.VisibilityOff, ""
+                    )
+                }
+            }
         )
         Button(
             modifier = Modifier
